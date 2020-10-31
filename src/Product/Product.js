3226ec65dbd35b85e5517c2b02fce483cadb79f3
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-31 14:39:31 +0100

    wip

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -6,20 +6,18 @@ const Product = () => {
   return (
     <div className="product">
       <div className="product__info">
-        <p>the lean startup</p>
+        <p>The lean startup</p>
         <p className="product__price">
           <small>$</small>
           <strong>19.99</strong>
         </p>
         <div className="product__rating">
           <p> ⭐ </p>
-          <p> ⭐ </p>
-          <p> ⭐ </p>
-
         </div>
       </div>
       <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
       alt="lean-startup" />
+      <button>Add to Basket</button>
     </div>
   );
 }
