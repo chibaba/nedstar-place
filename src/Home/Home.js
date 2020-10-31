2672813cc52886be3dd59102eff16d1f608af7d0
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-31 21:48:27 +0100

    wip

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -25,7 +25,7 @@ const Home = () => {
         title="KenWood Kmix stand Mixer for baking, styling kitchen Mixer with kbeater.Dough Hook and Whisk, 5 litre Glass Bowl"
         price={239.09}
         rating={4}
-        image="https://images-na.ssl-images-amazon.com/I/810%2BGNdkzKL._AC_SX450_.jpg"
+        image="https://www.220-electronics.com/media/catalog/product/k/e/kenwood-kmix-kmx51-mixer--up.jpg"
        />
 
 
@@ -37,7 +37,21 @@ const Home = () => {
         title="Samsung LC49RG92345SD 4' Curved Led Gaming Monitor"
         price={199.99}
         rating={3}
-        image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_jpg"
+        image="https://static.toiimg.com/photo/77588460.cms"
+      />
+       <Product 
+        id="fe323dfe"
+        title="Amazon Echo (3rd Generation) |
+        Smart speaker with Alexa, Charcoal fabric"
+        price={98.23}
+        rating={4}
+        image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+      /> <Product 
+        id="cfg33dfe"
+        title="New apple ipad Pro (12.9-inch,Wi-Fi, 128GB) - Silver (4th Generation)"
+        price={599.99}
+        rating={5}
+        image="https://www.apple.com/newsroom/images/product/airpods/standard/Apple_AirPods-Pro_New-Design_102819_LP_hero.jpg.og.jpg"
       />
    
 
