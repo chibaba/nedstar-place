93372453910824478f8517fb27e38578e4317da1
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-01 15:43:13 +0100

    adding react currency converter

--- src/checkout/Subtotal.js
+++ src/checkout/Subtotal.js
@@ -1,3 +1,4 @@
+import { ShoppingBasket } from '@material-ui/icons';
 import React from 'react';
 import CurrencyFormat  from 'react-currency-format'
 
@@ -7,6 +8,24 @@ import './Subtotal.css'
 const Subtotal = () => {
   return (
     <div className="subtotal">
+    <CurrencyFormat
+    renderText={(value) => (
+      <>
+      <p> 
+          Subtotal({ShoppingBasket.length} items):
+          <strong>{` ${value}`}</strong>
+      </p>
+      <small className="subtotal__gift">
+        <input type="checkbox" /> This Order contain a gift
+      </small>
+      </>
+    )}
+    decimalScale={2}
+    value={getBasketTotal(basket)}
+    displayType={"text"}
+    thosandSeperator={truue}
+    prefix={"#"}
+    />
       
     </div>
   );
