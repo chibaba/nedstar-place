81d9d74e41e2ee6f37240d54322cb77402ee4e28
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-01 10:16:24 +0100

    wip

--- src/checkout/Checkout.js
+++ src/checkout/Checkout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './Checkout.css'
+import Subtotal from './Subtotal';
 
 const Checkout = () => {
   return (
---
+++ src/checkout/Subtotal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CurrencyFormat  from 'react-currency-format'
+
+import './Subtotal.css'
+
+
+const Subtotal = () => {
+  return (
+    <div className="subtotal">
+      
+    </div>
+  );
+}
+
+export default Subtotal;
