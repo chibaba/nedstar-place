d129a9d47e201a5223536847b78c393484521988
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-02 08:10:33 +0100

    wip

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
 
 // Selector
 export const getBasketTotal = (basket) =>
-basket?.reduce((amount, item) => item.price + amount,  )
+basket?.reduce((amount, item) => item.price + amount,  0)
 const reducer = (state, action) => {
   console.log(action);
   switch(action.type) {
