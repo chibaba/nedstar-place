96199a6bee48232a859cebc9430cc0b9bc8e3760
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-04 00:00:51 +0100

    wip

--- src/Signup/Login.js
+++ src/Signup/Login.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import logo from '../logo/logo-ned.png' 
+
+
 import './Login.css'
 
 const Login = () => {
   return (
     <div className='login'>
+    <img src={logo} alt="nedstar" />
       
     </div>
   );
